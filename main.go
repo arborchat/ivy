94acab0ec7021fa21007daf20655d69b032c579b
Josh Whetton <jtwhetto@us.ibm.com>
2018-09-05 21:51:05 -0400

    Moved the anon function out of main and gave it a temporary name.

--- main.go
+++ main.go
@@ -2,77 +2,82 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
 )
 
 // Connectivity constants
 const (
-    proto = "tcp"
-    serv = "localhost:7777"
-    welcome = "\n--| ~Ivy~ Version: XXX |--\n"
+	proto   = "tcp"
+	serv    = "localhost:7777"
+	welcome = "\n--| ~Ivy~ Version: XXX |--\n"
 )
 
 // Error handler
 func handleit(e error) {
-    if e != nil {
-        fmt.Println("OH NO: %s", e)
-    }
+	if e != nil {
+		fmt.Printf("OH NO: %s", e)
+	}
 }
 
 // Messages as defined in the Arbor protocol
 type Message struct {
-    Type      int
-    Root      string
-    Parent    string
-    UUID      string
-    Recent    []string
-    Content   string
-    Username  string
-    Timestamp int
-    Major     int
-    Minor     int
+	Type      int
+	Root      string
+	Parent    string
+	UUID      string
+	Recent    []string
+	Content   string
+	Username  string
+	Timestamp int
+	Major     int
+	Minor     int
 }
 
 func main() {
-    // MOTD
-    fmt.Println(welcome)
-
-	// Declare messages
-	var m Message
+	// MOTD
+	fmt.Println(welcome)
 
 	// Server connection
-    conn, err := net.Dial(proto, serv)
+	conn, err := net.Dial(proto, serv)
 	handleit(err)
-
-
-	// JSON decoder
-	listener := json.NewDecoder(conn)
-
-
-	// Get root message from the server
-    fmt.Println("~~ Message from server ~~")
-	e := listener.Decode(&m)
-	handleit(e)
-	fmt.Printf("Type: %d\nRoot Message: %s\nRecent Messages: %v\nServer Version %d.%d\n", m.Type, m.Root, m.Recent, m.Major, m.Minor)
 
 	// Create channel
 	rcvChan := make(chan Message)
 
-	go func() {
-        // Get stuff from the server and send to channel
-        for {
-        	le := listener.Decode(&m)
-        	handleit(le)
-        	rcvChan <- m
-        }
-	}()
+	go something(rcvChan, conn)
 
 	// Print stuff from the server sent through the channel
+	for incMessage := range rcvChan {
+		fmt.Println("~~ Message from server ~~")
+		fmt.Printf("Type: %d\nParent: %s\nContent: %s\n", incMessage.Type, incMessage.Parent, incMessage.Content)
+	}
+}
+
+func something(rcvChan chan<- Message, conn io.Reader) {
+	defer close(rcvChan)
+
+	var m Message
+
+	// JSON decoder
+	listener := json.NewDecoder(conn)
+
+	// Get root message from the server
+	fmt.Println("~~ Message from server ~~")
+	e := listener.Decode(&m)
+	handleit(e)
+	fmt.Printf("Type: %d\nRoot Message: %s\nRecent Messages: %v\nServer Version %d.%d\n", m.Type, m.Root, m.Recent, m.Major, m.Minor)
+
+	// Get stuff from the server and send to channel
 	for {
-    	incMessage := <-rcvChan
-        fmt.Println("~~ Message from server ~~")
-    	fmt.Printf("Type: %d\nParent: %s\nContent: %s\n", incMessage.Type, incMessage.Parent, incMessage.Content)
+		le := listener.Decode(&m)
+		// add handling for the errors?
+		if le == io.EOF {
+			fmt.Println("Server closed connection...")
+			return
+		}
+		rcvChan <- m
 	}
 }
